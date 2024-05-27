99522b0bc9b21df710617c562ec2831dd7bb9a48
Riya Kulkarni <riya.kulkarni.2803@gmail.com>
2024-05-27 21:32:02 +0530

    base-commit

---
+++ android/app/src/main/kotlin/com/youneedthis/likh_lena_chahiye_tha/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.youneedthis.likh_lena_chahiye_tha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:likh_lena_chahiye_tha/firebase_options.dart';
+import 'package:likh_lena_chahiye_tha/views/login_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      // primarySwatch: Colors.blue,
+      useMaterial3: true,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ), 
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+              } else {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => const VerifyEmailView()));
+              }
+              return const Text("Done");
+            default:
+              return const Text("Loading");
+          }
+        },),
+    );
+  }
+}
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({super.key});
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
